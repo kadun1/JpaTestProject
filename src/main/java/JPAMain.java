2931933808005bf9ae354c3235f448e2f5e5292a
kadun1 <kadunjj@gmail.com>
2023-01-23 02:01:35 +0900

    Initial commit

---
+++ src/main/java/JPAMain.java
@@ -0,0 +1,26 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JPAMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("kadun");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+
+
+            tx.commit();
+
+        } catch (Exception e) {
+            System.out.println("Error Occurred");
+            e.printStackTrace();
+        }
+    }
+}
